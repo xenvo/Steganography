0af361ff11eeed1cd87b5eb3e7d26e547b685c7f
xenvo <54676855+xenvo@users.noreply.github.com>
2021-12-15 21:55:01 +0700

    Update stegoDecode.py

--- stegoDecode.py
+++ stegoDecode.py
@@ -2,19 +2,16 @@ import base64
 import urllib.request
 from stegano import lsb
 
-#Meminta user input untuk link image Imgur
-imgurLink = input("Insert IMGUR Image Link: ")
-
-#Mengambil gambar dari link Imgur yang tadi diinput
+imgurLink = input("Insert IMGUR image link: ")
 urllib.request.urlretrieve(imgurLink, "lesterDecode.png")
-
-#Mengambil message dari file PNG yang sudah ditarik dari Imgur
 image_decode = lsb.reveal("lesterDecode.png")
-
-print("\nENCRYPTED MESSAGE FOUNDED!!")
-print("This is the encrypted message: " + image_decode)
-
-#Melakukan decoding dari encrypted message
+print("\n[!]ENCRYPTED MESSAGE FOUND!!")
+print("The encrypted message is " + image_decode)
 msg_encrypted = image_decode.encode("utf-8")
 msg_decode = base64.b64decode(msg_encrypted)
-print(f"\nDecode Message: " + msg_decode.decode("utf-8"))
+print(f"\nDecode message: " + msg_decode.decode("utf-8"))
+
+#Source:
+#https://stackoverflow.com/questions/8286352/how-to-save-an-image-locally-using-python-whose-url-address-i-already-know
+#https://pythonrepo.com/repo/Damgaard-PyImgur-python-third-party-apis-wrappers
+#https://www.geeksforgeeks.org/encoding-and-decoding-base64-strings-in-python/
