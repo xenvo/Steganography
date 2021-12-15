7ea1a3567ee32601a329e4b52670591ef869ac46
xenvo <54676855+xenvo@users.noreply.github.com>
2021-12-15 21:53:12 +0700

    Update stegoUpload.py

--- stegoUpload.py
+++ stegoUpload.py
@@ -4,29 +4,24 @@ import requests
 import json
 from stegano import lsb
 
-#Melakukan Encode Message ke dalam Base 64
 msg = input("Message to encode with UTF-8: ")
 msg_utf8 = msg.encode("utf-8")
 msg_b64encoded = base64.b64encode(msg_utf8)
-
 print(f"Encoded string to Base64: " + msg_b64encoded.decode("utf-8"))
-
 msg_b64decoded = msg_b64encoded.decode("utf-8")
-
-#memasukan message kedalam file PNG.
-image = lsb.hide("lester.png", msg_b64decoded) #image source
-image.save("lesterStego.png") #image yang sudah di inject dengan message dunamakan bananaSTEG.png
+image = lsb.hide("lester.png", msg_b64decoded)
+image.save("lesterStego.png")
 image_decode = lsb.reveal("lesterStego.png")
 print(image_decode) 
 print("\nMessage succesfully injected")
-
-#deklarasi API Key Imgur dan file PNG yang sudah diinject dengan message
 CLIENT_ID = "82163dae73db44e"
 PATH = "lesterStego.png"
-
-#upload file PNG ke Imgur
 imageUpload = pyimgur.Imgur(CLIENT_ID)
 uploaded_image = imageUpload.upload_image(PATH, title="Stegano")
-
-#print link imgur dari file PNG
 print(uploaded_image.link)
+
+#Source:
+#https://pythonrepo.com/repo/Damgaard-PyImgur-python-third-party-apis-wrappers
+#https://www.geeksforgeeks.org/encoding-and-decoding-base64-strings-in-python/
+#https://www.youtube.com/watch?v=lvky-tmoTvg
+#https://www.geeksforgeeks.org/image-based-steganography-using-python/
